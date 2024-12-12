5c87f6459ba4400870231278c5a447c90d2ad139
Eun Oh <bdohhhhh@gmail.com>
2024-12-12 15:40:30 +0900

    [update]: 241212 15:40

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen bg-neutral-200 flex justify-center items-center">
-      <div className="w-20 h-20 bg-red-400">
+      <div className="w-20 h-60 bg-red-400 flex justify-center items-center flex-col">
         <div className="w-20 h-20 bg-blue-400"></div>
         <div className="w-20 h-20 bg-green-400"></div>
         <div className="w-20 h-20 bg-yellow-400"></div>
