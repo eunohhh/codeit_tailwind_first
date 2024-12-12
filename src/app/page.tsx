516ecbc93feeeeb1537cd845f15687e61beea145
Eun Oh <bdohhhhh@gmail.com>
2024-12-12 14:39:58 +0900

    [script]: 프리티어 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <div className="w-full h-screen bg-neutral-200 flex justify-center items-center">
-      <div className="container bg-red-400">
+      <div className="w-20 h-20 bg-red-400">
         <div className="w-20 h-20 bg-blue-400"></div>
+        <div className="w-20 h-20 bg-green-400"></div>
+        <div className="w-20 h-20 bg-yellow-400"></div>
       </div>
     </div>
   );
