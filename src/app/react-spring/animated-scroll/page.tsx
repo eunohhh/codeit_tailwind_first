223a4bac67666877463d1de11b28f0c943fb01ca
Eun Oh <bdohhhhh@gmail.com>
2024-12-17 11:05:25 +0900

    [add]: animated-scroll

---
+++ src/app/react-spring/animated-scroll/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { animated, useScroll } from '@react-spring/web';
+
+function ReactSpringAnimatedScrollPage() {
+  const props = useScroll();
+
+  console.log(props);
+
+  return (
+    <div>
+      <div className="w-dvw h-[200vh]">
+        <animated.div
+          className="h-1 bg-red-500 fixed top-0 left-0"
+          style={{ width: props.scrollYProgress.to((x) => `${x * 100}%`) }}
+        ></animated.div>
+        <animated.div style={{ transform: props.scrollY.to((y) => `translateY(${y}px)`) }}>
+          <h1>Hello</h1>
+        </animated.div>
+      </div>
+    </div>
+  );
+}
+
+export default ReactSpringAnimatedScrollPage;
