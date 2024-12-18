8a79739e0d623701f2eff52cfa900db3122f0846
Eun Oh <bdohhhhh@gmail.com>
2024-12-18 11:45:21 +0900

    [update]: scrolling-wave 완료

--- src/app/framer-motion/scrolling-wave/page.tsx
+++ src/app/framer-motion/scrolling-wave/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion';
+import { motion, useMotionValueEvent, useScroll, useSpring, useTransform } from 'framer-motion';
 import { useCallback, useState } from 'react';
 
 const X_LINES = 40;
@@ -17,7 +17,10 @@ function ScrollingWavePage() {
     [0, 1],
     ['circle(0% at 50% 50%)', 'circle(100% at 50% 50%)'],
   );
-  const textMove = useTransform(scrollYProgress, [0, 0.7, 1], ['100%', '100%', '0%']);
+  // useMotionValue 는 set시 애니메이션 없이 즉시 바뀜
+  // useSpring 은 set시 애니메이션 있이 바뀜
+  // const textMove = useTransform(scrollYProgress, [0, 0.7, 1], ['100%', '100%', '0%']);
+  const textValue = useSpring(0, { bounce: 0.1 });
 
   const calculateWidth = useCallback((where: 'left' | 'right', scrollP: number) => {
     return Array.from({ length: X_LINES }).map((_, i) => {
@@ -29,54 +32,56 @@ function ScrollingWavePage() {
   }, []);
 
   const [leftVarWidths, setLeftVarWidths] = useState<number[]>(calculateWidth('left', 0));
-
   const [rightVarWidths, setRightVarWidths] = useState<number[]>(calculateWidth('right', 0));
 
   useMotionValueEvent(scrollYProgress, 'change', (scrollP) => {
     setLeftVarWidths(calculateWidth('left', scrollP));
-
     setRightVarWidths(calculateWidth('right', scrollP));
+    if (scrollP > 0.7) {
+      textValue.set(0);
+    } else {
+      textValue.set(300);
+    }
   });
+
+  // 아래 useTransform 은 굳이 안하고 useSpring 값 바로 써도 똑같음
+  // const textStyle = useTransform(textValue, () => `${textValue.get()}%`);
 
   return (
     <>
       <div className="w-full h-full fixed flex inset-0 pointer-events-none z-10">
         <motion.div className="w-full h-full flex flex-col items-start z-10 justify-between">
-          {leftVarWidths.map((width, i) => {
-            return (
-              <motion.div
-                key={i}
-                className="h-[1vh] bg-slate-300"
-                style={{
-                  width,
-                }}
-              />
-            );
-          })}
+          {leftVarWidths.map((width, i) => (
+            <motion.div
+              key={i}
+              className="h-[1vh] bg-slate-300"
+              style={{
+                width,
+              }}
+            />
+          ))}
         </motion.div>
         <motion.div className="w-full h-full flex flex-col items-end z-10 justify-between">
-          {rightVarWidths.map((width, i) => {
-            return (
-              <motion.div
-                key={i}
-                className="h-[1vh] bg-slate-300"
-                style={{
-                  width,
-                }}
-              />
-            );
-          })}
+          {rightVarWidths.map((width, i) => (
+            <motion.div
+              key={i}
+              className="h-[1vh] bg-slate-300"
+              style={{
+                width,
+              }}
+            />
+          ))}
         </motion.div>
       </div>
       <motion.div className="bg-orange-400 fixed top-0 left-0 w-full h-full" style={{ clipPath }}>
         <h1 className="text-blue-600 font-bold text-[8vw] pl-[8vw]">
           <span className="block overflow-hidden">
-            <motion.span className="block" style={{ y: textMove }}>
+            <motion.span className="block" style={{ y: textValue }}>
               Aha!
             </motion.span>
           </span>
           <span className="block overflow-hidden">
-            <motion.span className="block" style={{ y: textMove }}>
+            <motion.span className="block" style={{ y: textValue }}>
               You found me!
             </motion.span>
           </span>
